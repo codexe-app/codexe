9282d797e7483c8e699be53a560380a1cfbdf64a
Rex Wesley Reyes <rreyes@launchbrigade.com>
2024-04-21 07:52:21 -0700

    Graphql Tweaks

---
+++ src/graphql/custom.ts
@@ -0,0 +1,48 @@
+import * as APITypes from "./API";
+type GeneratedQuery<InputType, OutputType> = string & {
+  __generatedQueryInput: InputType;
+  __generatedQueryOutput: OutputType;
+};
+
+export const fullDocuments = /* GraphQL */ `query ListDocuments(
+    $filter: ModelDocumentFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listDocuments(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        slug
+        description
+        content
+        status
+        topicId
+        userId
+        createdAt
+        updatedAt
+        topic {
+          name
+          description
+        }
+        user {
+          firstname
+          lastname
+          username
+          email
+          avatar
+          }
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
+      }
+      nextToken
+      startedAt
+      __typename
+    }
+  }
+  ` as GeneratedQuery<
+    APITypes.ListDocumentsQueryVariables,
+    APITypes.ListDocumentsQuery
+  >;
