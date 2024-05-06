e691afb44a5a6240f7fbda808ab1eedb9c57185b
Rex Wesley Reyes <rreyes@launchbrigade.com>
2024-05-06 08:02:35 -0700

    Backup image incase APOD is a video

--- src/app/[username]/create.tsx
+++ src/app/[username]/create.tsx
@@ -5,6 +5,13 @@ import classes from './dash.module.css'
 export default async function CreateCard(props: any) {
   const user = props.user
   const image = await getData()
+  const file = image.url
+  const fileType = file['type']
+  const validImageTypes = ['image/gif', 'image/jpeg', 'image/png']
+  if (!validImageTypes.includes(fileType)) {
+    console.log(`INVALID`)
+    image.url = 'https://apod.nasa.gov/apod/image/2405/BhShredder_NASA_1080.jpg'
+  }
 
   async function getData() {
     const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
