0601c660015a193f5a7c0da095cc11b376093917
Rex Wesley Reyes <rreyes@launchbrigade.com>
2024-05-10 15:53:43 -0700

    Left over progress attempt

--- src/app/shell.tsx
+++ src/app/shell.tsx
@@ -2,12 +2,10 @@
 import Link from 'next/link'
 import { AppShell, Group, Button } from '@mantine/core'
 import { HorizontalLogo } from '@/app/logo'
-import { RouterTransition } from '@/utils/routertrans'
 
 export default function Shell({ children }: { children: any }): JSX.Element {
   return (
     <AppShell header={{ height: 48 }}>
-      <RouterTransition />
       <AppShell.Header>
         <Group h='100%' px='sm'>
           <Group justify='space-between' style={{ flex: 1 }}>
