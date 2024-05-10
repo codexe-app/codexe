39f26af0518a8403df8fe2c6239e9c62ad713085
Rex Wesley Reyes <rreyes@launchbrigade.com>
2024-05-10 13:38:55 -0700

    Adding MDX

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
 };
 
-export default nextConfig;
+import withMDX from "@next/mdx";
+
+export default withMDX({
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  }, 
+})(nextConfig);
