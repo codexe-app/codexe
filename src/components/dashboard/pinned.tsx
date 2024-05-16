6f1e669c7a7b9ca5e45e2f8a02342b2773bdaa29
Rex Wesley Reyes <rreyes@launchbrigade.com>
2024-05-16 11:50:06 -0700

    Fixes for undefined content

--- src/components/dashboard/pinned.tsx
+++ src/components/dashboard/pinned.tsx
@@ -17,7 +17,7 @@ export default function PinnedCard(props: any) {
   const { user, pinned } = props
 
   function getPath(type: any) {
-    return type.toLowerCase() + 's'
+    return type?.toLowerCase() + 's'
   }
 
   async function trashDocument(doc: any) {
@@ -40,7 +40,7 @@ export default function PinnedCard(props: any) {
   return (
     <Card shadow='xs' padding='sm' withBorder w='100%' h='240px'>
       <Box pos='absolute' top='-4px' right='0'>
-        <Badge radius='sm'>{pinned.status}</Badge>
+        <Badge radius='sm'>{pinned?.status}</Badge>
       </Box>
       <Group justify='start' wrap='nowrap' gap='xs'>
         <IconPinned color='var(--mantine-primary-color-filled)' />
@@ -49,26 +49,26 @@ export default function PinnedCard(props: any) {
       <Card.Section flex='1'>
         <Group justify='start' wrap='nowrap' mt='sm'>
           <AspectRatio ratio={1 / 1} w='140px'>
-            <Image src={pinned.graphic?.url} />
+            <Image src={pinned?.graphic?.url} />
           </AspectRatio>
           <Stack align='start'>
             <Group>
               <Stack gap={0} align='center'>
                 <Text tt='uppercase' lh={1} fz='.9rem' fw='600' c='var(--mantine-primary-color-filled)'>
-                  {dayjs(pinned.updatedAt).format('MMMM')}
+                  {dayjs(pinned?.updatedAt).format('MMMM')}
                 </Text>
                 <Title order={3} lh={0.75} c='var(--mantine-primary-color-filled)'>
-                  {dayjs(pinned.updatedAt).format('D')}
+                  {dayjs(pinned?.updatedAt).format('D')}
                 </Title>
               </Stack>
               <Title order={5} lh={1}>
-                {pinned.name}
+                {pinned?.name}
               </Title>
             </Group>
             <Text fz='xs' lh={1.25} c='gray.6'>
-              {pinned.description}
+              {pinned?.description}
             </Text>
-            <Badge>{pinned.__typename}</Badge>
+            <Badge>{pinned?.__typename}</Badge>
           </Stack>
         </Group>
       </Card.Section>
@@ -83,7 +83,7 @@ export default function PinnedCard(props: any) {
                   title: (
                     <Group>
                       <IconAlertCircle />
-                      <Title order={4}>{pinned.name}</Title>
+                      <Title order={4}>{pinned?.name}</Title>
                     </Group>
                   ),
                   children: <Text size='sm'>Are you sure you would like to delete this document?</Text>,
@@ -95,20 +95,20 @@ export default function PinnedCard(props: any) {
               }}>
               <IconTrash style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
             </ActionIcon>
-            <ActionIcon variant='outline' component={Link} href={`${user.username}/${getPath(pinned.__typename)}/${pinned.slug}`} color='var(--mantine-primary-color-6)'>
+            <ActionIcon variant='outline' component={Link} href={`${user.username}/${getPath(pinned?.__typename)}/${pinned?.slug}`} color='var(--mantine-primary-color-6)'>
               <IconPencil style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
             </ActionIcon>
             <ActionIcon
               variant='outline'
               onClick={() => {
                 modals.open({
-                  title: `${pinned.name} - ${pinned.updatedAt}`,
+                  title: `${pinned?.name} - ${pinned?.updatedAt}`,
                   size: 'xl',
                   padding: 'md',
                   fullScreen: true,
                   centered: true,
                   zIndex: 1000,
-                  children: <DocumentView content={pinned.content} />,
+                  children: <DocumentView content={pinned?.content} />,
                 })
               }}>
               <IconEye style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
